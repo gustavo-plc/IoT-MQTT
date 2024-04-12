d0e6a63a9fcacce8d7a16e8edf179c67e18bb78f
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-04-12 08:15:50 -0300

    inserções do código do projeto IA

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
+//carregamento das bibliotecas a serem utilizadas:
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <time.h>
+#include "DHT.h"
+
+// //nomeação dos pinos do hardware ESP32
+// #define O_IRRIGA 32 //variável de saída fuzzy: fechamento do relé e acionamento da irrigação. Pino 32
+// #define I_TEMPUMI 4 //variável de entrada 1 fuzzy: sensor de temperatura/umidade: um só sensor fornecerá dados para duas variáveis de entrada: temperatura e umidade. Pino 4
+// #define I_UMISOLO 34 //variável de entrada 2 fuzzy: sensor de umidade do solo. Pino 34
+// #define DHTTYPE DHT11
 
 // informações da rede wi-fi
 const char *ssid = "Gustavo's Galaxy M22";
@@ -11,6 +23,8 @@ const char *broker = "b37.mqtt.one";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+DHT dht(I_TEMPUMI, DHTTYPE);   //inicializando o sensor. 
 
 // funções de configuração
 void setupWiFi() {
@@ -43,6 +57,16 @@ void reconnect() {
 }
 
 void setup() {
+  // //CONFIGURAÇÃO DE CADA PINO DEFINIDO ANTERIORMENTE PARA FUNCIONAREM COMO ENTRADAS OU SAÍDAS
+  // pinMode(O_IRRIGA,OUTPUT);
+  // pinMode(I_TEMPUMI,INPUT);
+  // pinMode(I_UMISOLO,INPUT);
+
+  // // INICIALIZAÇÃO DA TRANSMISSÃO SERIAL NA TAXA DE 9600 bits por segundo
+  // Serial.begin(9600);
+  // Serial.println(F("Teste do DHT11!"));
+  // dht.begin();
+
   Serial.begin(115200);
   setupWiFi();
   client.setServer(broker, 1883); //local para inserção da porta para conexão
@@ -52,6 +76,19 @@ void loop() {
   if (!client.connected())
     reconnect();
   client.loop();
+
+  // digitalWrite(O_IRRIGA, LOW);
+  // delay(2000);
+  // //leitura de umidade do ar e temperatura do sensor DHT11
+  // float umidadeAr = dht.readHumidity();
+  // float temperatura = dht.readTemperature();
+
+  // // Verificação de erros de leitura e informação via terminal
+  // if (isnan(umidadeAr) || isnan(temperatura)) {
+  //   Serial.println(F("Falha em ler dados do sensor!\n"));
+  //   return;
+  // }
+
 }
 
 
