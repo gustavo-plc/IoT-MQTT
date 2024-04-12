7794be4bfda7579d2b3c53de0c92fb4a14b53a50
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-04-12 14:46:28 -0300

    alterações de biblioteca

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,7 @@
 #include <PubSubClient.h>
 #include <WiFi.h>
 #include "DHT.h"
-#include "SPIFFS.h"
-#include <CertStoreBearSSL.h>
 #include <WiFiClientSecure.h>
-
 
 #define LED 32
 #define SENSOR 4
@@ -25,6 +22,30 @@ unsigned long lastMsg = 0;
 char msg[MSG_BUFFER_SIZE];
 
 dht DHT;
+
+const char* root_ca = 
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n" \
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n" \
+"QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n" \
+"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n" \
+"b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n" \
+"9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n" \
+"CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n" \
+"nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n" \
+"43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n" \
+"T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n" \
+"gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n" \
+"BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n" \
+"TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n" \
+"DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n" \
+"hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n" \
+"06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n" \
+"PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n" \
+"YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n" \
+"CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n" \
+"-----END CERTIFICATE-----\n";
 
 void setupWiFi() {
   delay(1000);
@@ -85,26 +106,7 @@ void setup() {
   pinMode(LED, OUTPUT);
   pinMode(SENSOR, INPUT);
 
-  if (!SPIFFS.begin(true)) {
-    Serial.println("Erro ao montar sistema de arquivos SPIFFS!");
-    return;
-  }
-
-  File caFile = SPIFFS.open("/emqxsl-ca.crt", "r");
-  if (!caFile) {
-    Serial.println("Erro ao abrir arquivo de certificado CA!");
-    return;
-  }
-
-  size_t size = caFile.size();
-  uint8_t certBuf[size];
-  size_t bytesRead = caFile.read(certBuf, size);
-  if (bytesRead != size) {
-    Serial.println("Erro ao ler o arquivo de certificado CA!");
-    return;
-  }
-
-  espClient.setTrustAnchors(new CertStoreBearSSL(certBuf, size));
+  espClient.setCACert(root_ca);
 
   client.setServer(broker, 8883);
   client.setCallback(callback);
@@ -139,10 +141,4 @@ void loop() {
   if (!client.connected())
     reconnect();
   client.loop();
-}
-
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
