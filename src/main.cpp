530a506afff20908784287b7ef18155b46e6877d
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-04-16 15:13:54 -0300

    atualização de código para não exibir a string "Message receive" toda vez que ocorrer uma publicação.

--- src/main.cpp
+++ src/main.cpp
@@ -89,9 +89,6 @@ void reconnect() {
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
   for (int i = 0; i < length; i++)
     Serial.print((char)payload[i]);
   Serial.println();
@@ -114,8 +111,6 @@ void callback(char *topic, byte *payload, unsigned int length) {
     client.publish("iot/lampada", msg);
   }
 }
-
-
 
 void setup() {
   pinMode(LED, OUTPUT);
