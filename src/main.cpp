f6bb0344e40266acc75f2142dc505cd14ac38693
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-06-17 10:21:15 -0300

    servos funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ char customKey;
 const byte ROWS = 4;
 const byte COLS = 4;
 char hexaKeys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
+  {'1', '2', '3', 'A'}, 
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
